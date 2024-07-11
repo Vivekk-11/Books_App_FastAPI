141645d6f0da3ba876aadb31b906fcab23b30420
vivek <chimnanivivek14@gmail.com>
2024-07-11 06:42:50 +0530

    all books endpoint

--- books.py
+++ books.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {"title": "Book One", "author": "Author One", "category": "science"},
+    {"title": "Book Two", "author": "Author Two", "category": "science"},
+    {"title": "Book Three", "author": "Author Three", "category": "history"},
+    {"title": "Book Four", "author": "Author Four", "category": "math"},
+    {"title": "Book Five", "author": "Author Five", "category": "math"},
+    {"title": "Book Six", "author": "Author Two", "category": "math"},
+]
 
-@app.get("/")
-async def first_api():
-    return {"message": "Hello, Vivek!"}
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
